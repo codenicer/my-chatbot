b1cbad0a14d846302245c72bc8b0db34cd2b7d91
codeNicer <ruthertenido@gmail.com>
2025-02-02 20:54:53 +0800

    fix: tailwind config

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,30 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
 
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
-  presets: [sharedConfig],
-};
+const config: Config = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [animate],
+}
 
-export default config;
+export default config
