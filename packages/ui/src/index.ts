86b3bf273baa71f967282984790499d6865ad7db
codeNicer <ruthertenido@gmail.com>
2025-02-02 20:57:54 +0800

    jumpingout from ui for meantime -  proceeding to adding chatbot core

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,9 @@
+export * from './components/chat-widget'
+export * from './components/chat-message'
+export * from './components/chat-message-list'
+export * from './components/ui/button'
+export * from './components/ui/card'
+export * from './components/ui/dialog'
+export * from './components/ui/input'
+export * from './components/ui/scroll-area'
+// We'll add more exports as we create more components
